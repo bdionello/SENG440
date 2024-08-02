50fa2b1ba956ccf97e719bf18aac40b4e9898957
Demo User <debian@beaglebone.localdomain>
2024-08-02 18:22:46 +0000

    re-compiled V3-6 without -O3 flag

--- filterV5_LoopUnrollFour/filter.s
+++ filterV5_LoopUnrollFour/filter.s
@@ -6,219 +6,563 @@
 	.eabi_attribute 24, 1
 	.eabi_attribute 25, 1
 	.eabi_attribute 26, 2
-	.eabi_attribute 30, 2
+	.eabi_attribute 30, 6
 	.eabi_attribute 34, 1
 	.eabi_attribute 18, 4
 	.file	"filter.c"
 	.text
-	.section	.text.startup,"ax",%progbits
+	.comm	X,256,4
+	.comm	Y,256,4
 	.align	1
-	.p2align 2,,3
-	.global	main
 	.arch armv7-a
+	.syntax unified
+	.thumb
+	.thumb_func
+	.fpu vfpv3-d16
+	.type	filter_init, %function
+filter_init:
+	@ args = 0, pretend = 0, frame = 8
+	@ frame_needed = 1, uses_anonymous_args = 0
+	@ link register save eliminated.
+	push	{r4, r7}
+	sub	sp, sp, #8
+	add	r7, sp, #0
+	str	r0, [r7, #4]
+	str	r1, [r7]
+	ldr	r3, [r7, #4]
+	movw	r2, #32769
+	strh	r2, [r3]	@ movhi
+	ldr	r3, [r7, #4]
+	adds	r3, r3, #2
+	movw	r2, #32769
+	strh	r2, [r3]	@ movhi
+	movs	r4, #2
+	b	.L2
+.L3:
+	mov	r3, r4
+	lsls	r3, r3, #1
+	ldr	r2, [r7, #4]
+	add	r3, r3, r2
+	movw	r2, #32767
+	strh	r2, [r3]	@ movhi
+	adds	r4, r4, #1
+.L2:
+	cmp	r4, #99
+	ble	.L3
+	ldr	r3, [r7]
+	mov	r2, #49152
+	strh	r2, [r3]	@ movhi
+	ldr	r3, [r7]
+	adds	r3, r3, #2
+	mov	r2, #49152
+	strh	r2, [r3]	@ movhi
+	nop
+	adds	r7, r7, #8
+	mov	sp, r7
+	@ sp needed
+	pop	{r4, r7}
+	bx	lr
+	.size	filter_init, .-filter_init
+	.section	.rodata
+	.align	2
+.LC0:
+	.ascii	"Y[ 0] = %+6hi = 0x%04hX ....... y[ 0] = %8.5f\012\000"
+	.align	2
+.LC1:
+	.ascii	"Y[ 1] = %+6hi = 0x%04hX ....... y[ 1] = %8.5f\012\000"
+	.align	2
+.LC2:
+	.ascii	"Y[%2d] = %+6hi = 0x%04hX ....... y[%2d] = %8.5f\012"
+	.ascii	"\000"
+	.text
+	.align	1
+	.global	main
 	.syntax unified
 	.thumb
 	.thumb_func
 	.fpu vfpv3-d16
 	.type	main, %function
 main:
-	@ args = 0, pretend = 0, frame = 16
-	@ frame_needed = 0, uses_anonymous_args = 0
-	push	{r4, r5, r6, r7, r8, r9, r10, fp, lr}
-	mvn	r1, #-2147450880
-	vpush.64	{d8}
-	movw	r4, #32769
-	ldr	r0, .L8+4
-	movt	r4, 32769
-	ldr	r3, .L8+8
+	@ args = 0, pretend = 0, frame = 96
+	@ frame_needed = 1, uses_anonymous_args = 0
+	push	{r4, r5, r6, r7, lr}
+	sub	sp, sp, #116
+	add	r7, sp, #16
+	ldr	r5, .L7+4
 .LPIC0:
-	add	r0, pc
-	sub	sp, sp, #36
-	ldr	r3, [r0, r3]
-	add	r2, r3, #200
-	mov	r7, r3
-	str	r4, [r3], #4	@ unaligned
-.L2:
-	str	r1, [r3], #4	@ unaligned
-	cmp	r3, r2
-	bne	.L2
-	ldr	r3, .L8+12
-	mov	r2, #49152
-	movt	r2, 65535
-	mov	r10, #0
-	mov	fp, #0
-	mov	r5, #-1073692672
-	ldr	r4, [r0, r3]
-	mov	r1, r2
-	ldr	r0, .L8+16
-	movt	fp, 49136
-	ldr	r3, .L8+20
-	str	r5, [r4]	@ unaligned
-.LPIC1:
-	add	r0, pc
-	strd	r10, [sp]
-.LPIC3:
-	add	r3, pc
-	str	r3, [sp, #24]
-	bl	printf(PLT)
-	ldrsh	r2, [r4, #2]
-	mov	fp, #2
-	vldr.32	s17, .L8
-	mov	r9, r4
-	ldr	r0, .L8+24
-	mov	r4, fp
-	mov	r1, r2
-	vmov	s15, r2	@ int
-.LPIC2:
-	add	r0, pc
-	vmov.f32	s16, s17
-	vcvt.f32.s32	s15, s15
-	vmul.f32	s15, s15, s17
+	add	r5, pc
+	movw	r3, #30384
+	strh	r3, [r7, #94]	@ movhi
+	movw	r3, #30384
+	strh	r3, [r7, #92]	@ movhi
+	movw	r3, #30384
+	strh	r3, [r7, #90]	@ movhi
+	movw	r3, #29863
+	strh	r3, [r7, #88]	@ movhi
+	movw	r3, #38103
+	strh	r3, [r7, #86]	@ movhi
+	ldr	r3, .L7+8
+	ldr	r3, [r5, r3]
+	mov	r1, r3
+	ldr	r3, .L7+12
+	ldr	r3, [r5, r3]
+	mov	r0, r3
+	bl	filter_init(PLT)
+	ldr	r3, .L7+8
+	ldr	r3, [r5, r3]
+	ldrsh	r3, [r3]
+	mov	r1, r3
+	ldr	r3, .L7+8
+	ldr	r3, [r5, r3]
+	ldrsh	r3, [r3]
+	mov	r2, r3
+	ldr	r3, .L7+8
+	ldr	r3, [r5, r3]
+	ldrsh	r3, [r3]
+	vmov	s15, r3	@ int
+	vcvt.f32.s32	s14, s15
+	vldr.32	s13, .L7
+	vdiv.f32	s15, s14, s13
 	vcvt.f64.f32	d7, s15
 	vstr.64	d7, [sp]
+	ldr	r3, .L7+16
+.LPIC1:
+	add	r3, pc
+	mov	r0, r3
 	bl	printf(PLT)
-	str	r7, [sp, #20]
-.L3:
-	ldr	r3, [sp, #20]
-	movw	r5, #30384
-	ldrsh	r6, [r9, #2]
-	mov	r0, #8388608
-	ldrh	r10, [r9]
-	movw	ip, #29863
-	ldrh	r2, [r3, #4]
-	mov	lr, #16384
-	ldrh	r1, [r3, #2]
-	add	r9, r9, #8
-	ldrh	r3, [r3]
-	smulbb	r2, r2, r5
-	smulbb	r1, r1, r5
-	add	fp, r2, #8388608
-	add	r2, r2, #4194304
-	smlabb	r8, r3, r5, r0
-	asr	fp, fp, #24
-	movw	r0, #38103
-	asrs	r2, r2, #23
-	uxth	r3, fp
-	mov	fp, #8192
-	smlabb	fp, r6, ip, fp
-	mov	r7, r3
-	smlabb	r10, r10, r0, lr
-	add	r3, r1, #4194304
-	str	r7, [sp, #28]
-	add	r1, r1, #8388608
-	add	r3, r7, r3, asr #23
-	ldr	r7, [sp, #20]
-	add	r3, r3, r8, asr #24
-	add	r3, r3, fp, asr #14
-	mov	fp, #8192
-	add	r3, r3, r10, asr #15
-	ldrh	r8, [r7, #6]
-	mov	r10, #0	@ movhi
-	strh	r10, [r9, #2]	@ movhi
-	sxth	r3, r3
-	strh	r3, [r9, #-4]	@ movhi
-	smulbb	r8, r8, r5
-	vmov	s15, r3	@ int
-	smlabb	r6, r6, r0, lr
-	vcvt.f32.s32	s14, s15
-	add	r10, r8, #8388608
-	add	r8, r8, #4194304
-	smlabb	r7, r3, ip, fp
-	add	r2, r2, r10, asr #24
-	add	r1, r2, r1, asr #24
-	ldr	r2, [sp, #20]
-	add	r6, r1, r6, asr #15
-	mov	r1, #8388608
-	smlabb	lr, r3, r0, lr
-	vmul.f32	s14, s14, s16
-	ldrh	fp, [r2, #8]!
-	add	r6, r6, r7, asr #14
-	mov	r0, #8192
-	ldr	r7, [sp, #28]
-	sxth	r6, r6
-	strh	r6, [r9, #-2]	@ movhi
-	smlabb	fp, fp, r5, r1
-	str	r2, [sp, #20]
-	smlabb	ip, r6, ip, r0
+	ldr	r3, .L7+8
+	ldr	r3, [r5, r3]
+	ldrsh	r3, [r3, #2]
+	mov	r1, r3
+	ldr	r3, .L7+8
+	ldr	r3, [r5, r3]
+	ldrsh	r3, [r3, #2]
 	mov	r2, r3
-	mov	r1, r4
-	ldr	r0, [sp, #24]
-	add	r10, r4, #1
-	adds	r5, r4, #2
-	add	fp, r7, fp, asr #24
-	adds	r6, r4, #3
-	add	fp, fp, r8, asr #23
-	vcvt.f64.f32	d7, s14
-	add	lr, fp, lr, asr #15
-	add	ip, lr, ip, asr #14
-	strh	ip, [r9]	@ movhi
-	str	r4, [sp]
-	adds	r4, r4, #4
-	vstr.64	d7, [sp, #8]
-	bl	printf(PLT)
-	ldrsh	r3, [r9, #-2]
-	ldr	r7, [sp, #24]
-	mov	r1, r10
-	str	r10, [sp]
+	ldr	r3, .L7+8
+	ldr	r3, [r5, r3]
+	ldrsh	r3, [r3, #2]
 	vmov	s15, r3	@ int
-	mov	r2, r3
-	mov	r0, r7
 	vcvt.f32.s32	s14, s15
-	vmul.f32	s14, s14, s16
-	vcvt.f64.f32	d7, s14
-	vstr.64	d7, [sp, #8]
+	vldr.32	s13, .L7
+	vdiv.f32	s15, s14, s13
+	vcvt.f64.f32	d7, s15
+	vstr.64	d7, [sp]
+	ldr	r3, .L7+20
+.LPIC2:
+	add	r3, pc
+	mov	r0, r3
 	bl	printf(PLT)
-	ldrsh	r3, [r9]
-	mov	r1, r5
-	str	r5, [sp]
-	mov	r0, r7
-	vmov	s15, r3	@ int
-	mov	r2, r3
-	vcvt.f32.s32	s14, s15
-	vmul.f32	s14, s14, s16
-	vcvt.f64.f32	d7, s14
-	vstr.64	d7, [sp, #8]
-	bl	printf(PLT)
-	ldrsh	r3, [r9, #2]
-	mov	r1, r6
-	str	r6, [sp]
-	mov	r0, r7
-	vmov	s15, r3	@ int
-	mov	r2, r3
-	vcvt.f32.s32	s14, s15
-	vmul.f32	s14, s14, s16
-	vcvt.f64.f32	d7, s14
-	vstr.64	d7, [sp, #8]
-	bl	printf(PLT)
-	cmp	r4, #102
-	bne	.L3
-	add	sp, sp, #36
-	@ sp needed
-	vldm	sp!, {d8}
-	pop	{r4, r5, r6, r7, r8, r9, r10, fp, pc}
-.L9:
-	.align	2
+	movs	r4, #2
+	b	.L5
 .L8:
-	.word	947912704
+	.align	2
+.L7:
+	.word	1182793728
 	.word	_GLOBAL_OFFSET_TABLE_-(.LPIC0+4)
+	.word	Y(GOT)
+	.word	X(GOT)
+	.word	.LC0-(.LPIC1+4)
+	.word	.LC1-(.LPIC2+4)
+.L6:
+	ldrsh	r3, [r7, #94]
+	ldr	r2, .L9+4
+	ldr	r2, [r5, r2]
+	ldrsh	r2, [r2, r4, lsl #1]
+	mul	r3, r2, r3
+	add	r3, r3, #8388608
+	asrs	r3, r3, #24
+	str	r3, [r7, #80]
+	ldrsh	r3, [r7, #92]
+	subs	r2, r4, #1
+	ldr	r1, .L9+4
+	ldr	r1, [r5, r1]
+	ldrsh	r2, [r1, r2, lsl #1]
+	mul	r3, r2, r3
+	add	r3, r3, #4194304
+	asrs	r3, r3, #23
+	str	r3, [r7, #76]
+	ldrsh	r3, [r7, #90]
+	subs	r2, r4, #2
+	ldr	r1, .L9+4
+	ldr	r1, [r5, r1]
+	ldrsh	r2, [r1, r2, lsl #1]
+	mul	r3, r2, r3
+	add	r3, r3, #8388608
+	asrs	r3, r3, #24
+	str	r3, [r7, #72]
+	ldrsh	r3, [r7, #86]
+	subs	r2, r4, #2
+	ldr	r1, .L9+8
+	ldr	r1, [r5, r1]
+	ldrsh	r2, [r1, r2, lsl #1]
+	mul	r3, r2, r3
+	add	r3, r3, #16384
+	asrs	r3, r3, #15
+	str	r3, [r7, #68]
+	ldrsh	r3, [r7, #88]
+	subs	r2, r4, #1
+	ldr	r1, .L9+8
+	ldr	r1, [r5, r1]
+	ldrsh	r2, [r1, r2, lsl #1]
+	mul	r3, r2, r3
+	add	r3, r3, #8192
+	asrs	r3, r3, #14
+	str	r3, [r7, #64]
+	ldr	r3, [r7, #80]
+	uxth	r2, r3
+	ldr	r3, [r7, #76]
+	uxth	r3, r3
+	add	r3, r3, r2
+	uxth	r2, r3
+	ldr	r3, [r7, #72]
+	uxth	r3, r3
+	add	r3, r3, r2
+	uxth	r2, r3
+	ldr	r3, [r7, #64]
+	uxth	r3, r3
+	add	r3, r3, r2
+	uxth	r2, r3
+	ldr	r3, [r7, #68]
+	uxth	r3, r3
+	add	r3, r3, r2
+	uxth	r3, r3
+	sxth	r2, r3
+	ldr	r3, .L9+8
+	ldr	r3, [r5, r3]
+	strh	r2, [r3, r4, lsl #1]	@ movhi
+	ldrsh	r3, [r7, #94]
+	adds	r2, r4, #1
+	ldr	r1, .L9+4
+	ldr	r1, [r5, r1]
+	ldrsh	r2, [r1, r2, lsl #1]
+	mul	r3, r2, r3
+	add	r3, r3, #8388608
+	asrs	r3, r3, #24
+	str	r3, [r7, #60]
+	ldrsh	r3, [r7, #92]
+	ldr	r2, .L9+4
+	ldr	r2, [r5, r2]
+	ldrsh	r2, [r2, r4, lsl #1]
+	mul	r3, r2, r3
+	add	r3, r3, #4194304
+	asrs	r3, r3, #23
+	str	r3, [r7, #56]
+	ldrsh	r3, [r7, #90]
+	subs	r2, r4, #1
+	ldr	r1, .L9+4
+	ldr	r1, [r5, r1]
+	ldrsh	r2, [r1, r2, lsl #1]
+	mul	r3, r2, r3
+	add	r3, r3, #8388608
+	asrs	r3, r3, #24
+	str	r3, [r7, #52]
+	ldrsh	r3, [r7, #86]
+	subs	r2, r4, #1
+	ldr	r1, .L9+8
+	ldr	r1, [r5, r1]
+	ldrsh	r2, [r1, r2, lsl #1]
+	mul	r3, r2, r3
+	add	r3, r3, #16384
+	asrs	r3, r3, #15
+	str	r3, [r7, #48]
+	ldrsh	r3, [r7, #88]
+	ldr	r2, .L9+8
+	ldr	r2, [r5, r2]
+	ldrsh	r2, [r2, r4, lsl #1]
+	mul	r3, r2, r3
+	add	r3, r3, #8192
+	asrs	r3, r3, #14
+	str	r3, [r7, #44]
+	ldr	r3, [r7, #60]
+	uxth	r2, r3
+	ldr	r3, [r7, #56]
+	uxth	r3, r3
+	add	r3, r3, r2
+	uxth	r2, r3
+	ldr	r3, [r7, #52]
+	uxth	r3, r3
+	add	r3, r3, r2
+	uxth	r2, r3
+	ldr	r3, [r7, #44]
+	uxth	r3, r3
+	add	r3, r3, r2
+	uxth	r2, r3
+	ldr	r3, [r7, #48]
+	uxth	r3, r3
+	add	r3, r3, r2
+	uxth	r2, r3
+	adds	r3, r4, #1
+	sxth	r1, r2
+	ldr	r2, .L9+8
+	ldr	r2, [r5, r2]
+	strh	r1, [r2, r3, lsl #1]	@ movhi
+	ldrsh	r3, [r7, #94]
+	adds	r2, r4, #2
+	ldr	r1, .L9+4
+	ldr	r1, [r5, r1]
+	ldrsh	r2, [r1, r2, lsl #1]
+	mul	r3, r2, r3
+	add	r3, r3, #8388608
+	asrs	r3, r3, #24
+	str	r3, [r7, #40]
+	ldrsh	r3, [r7, #92]
+	adds	r2, r4, #1
+	ldr	r1, .L9+4
+	ldr	r1, [r5, r1]
+	ldrsh	r2, [r1, r2, lsl #1]
+	mul	r3, r2, r3
+	add	r3, r3, #4194304
+	asrs	r3, r3, #23
+	str	r3, [r7, #36]
+	ldrsh	r3, [r7, #90]
+	ldr	r2, .L9+4
+	ldr	r2, [r5, r2]
+	ldrsh	r2, [r2, r4, lsl #1]
+	mul	r3, r2, r3
+	add	r3, r3, #8388608
+	asrs	r3, r3, #24
+	str	r3, [r7, #32]
+	ldrsh	r3, [r7, #86]
+	ldr	r2, .L9+8
+	ldr	r2, [r5, r2]
+	ldrsh	r2, [r2, r4, lsl #1]
+	mul	r3, r2, r3
+	add	r3, r3, #16384
+	asrs	r3, r3, #15
+	str	r3, [r7, #28]
+	ldrsh	r3, [r7, #88]
+	adds	r2, r4, #1
+	ldr	r1, .L9+8
+	ldr	r1, [r5, r1]
+	ldrsh	r2, [r1, r2, lsl #1]
+	mul	r3, r2, r3
+	add	r3, r3, #8192
+	asrs	r3, r3, #14
+	str	r3, [r7, #24]
+	ldr	r3, [r7, #40]
+	uxth	r2, r3
+	ldr	r3, [r7, #36]
+	uxth	r3, r3
+	add	r3, r3, r2
+	uxth	r2, r3
+	ldr	r3, [r7, #32]
+	uxth	r3, r3
+	add	r3, r3, r2
+	uxth	r2, r3
+	ldr	r3, [r7, #24]
+	uxth	r3, r3
+	add	r3, r3, r2
+	uxth	r2, r3
+	ldr	r3, [r7, #28]
+	uxth	r3, r3
+	add	r3, r3, r2
+	uxth	r2, r3
+	adds	r3, r4, #2
+	sxth	r1, r2
+	ldr	r2, .L9+8
+	ldr	r2, [r5, r2]
+	strh	r1, [r2, r3, lsl #1]	@ movhi
+	ldrsh	r3, [r7, #94]
+	adds	r2, r4, #3
+	ldr	r1, .L9+4
+	ldr	r1, [r5, r1]
+	ldrsh	r2, [r1, r2, lsl #1]
+	mul	r3, r2, r3
+	add	r3, r3, #8388608
+	asrs	r3, r3, #24
+	str	r3, [r7, #80]
+	ldrsh	r3, [r7, #92]
+	adds	r2, r4, #2
+	ldr	r1, .L9+4
+	ldr	r1, [r5, r1]
+	ldrsh	r2, [r1, r2, lsl #1]
+	mul	r3, r2, r3
+	add	r3, r3, #4194304
+	asrs	r3, r3, #23
+	str	r3, [r7, #76]
+	ldrsh	r3, [r7, #90]
+	adds	r2, r4, #1
+	ldr	r1, .L9+4
+	ldr	r1, [r5, r1]
+	ldrsh	r2, [r1, r2, lsl #1]
+	mul	r3, r2, r3
+	add	r3, r3, #8388608
+	asrs	r3, r3, #24
+	str	r3, [r7, #72]
+	ldrsh	r3, [r7, #86]
+	adds	r2, r4, #1
+	ldr	r1, .L9+8
+	ldr	r1, [r5, r1]
+	ldrsh	r2, [r1, r2, lsl #1]
+	mul	r3, r2, r3
+	add	r3, r3, #16384
+	asrs	r3, r3, #15
+	str	r3, [r7, #68]
+	ldrsh	r3, [r7, #88]
+	adds	r2, r4, #2
+	ldr	r1, .L9+8
+	ldr	r1, [r5, r1]
+	ldrsh	r2, [r1, r2, lsl #1]
+	mul	r3, r2, r3
+	add	r3, r3, #8192
+	asrs	r3, r3, #14
+	str	r3, [r7, #64]
+	ldr	r3, [r7, #20]
+	uxth	r2, r3
+	ldr	r3, [r7, #16]
+	uxth	r3, r3
+	add	r3, r3, r2
+	uxth	r2, r3
+	ldr	r3, [r7, #12]
+	uxth	r3, r3
+	add	r3, r3, r2
+	uxth	r2, r3
+	ldr	r3, [r7, #8]
+	uxth	r3, r3
+	add	r3, r3, r2
+	uxth	r2, r3
+	ldr	r3, [r7, #4]
+	uxth	r3, r3
+	add	r3, r3, r2
+	uxth	r2, r3
+	adds	r3, r4, #3
+	sxth	r1, r2
+	ldr	r2, .L9+8
+	ldr	r2, [r5, r2]
+	strh	r1, [r2, r3, lsl #1]	@ movhi
+	ldr	r3, .L9+8
+	ldr	r3, [r5, r3]
+	ldrsh	r3, [r3, r4, lsl #1]
+	mov	r2, r3
+	ldr	r3, .L9+8
+	ldr	r3, [r5, r3]
+	ldrsh	r3, [r3, r4, lsl #1]
+	mov	r1, r3
+	ldr	r3, .L9+8
+	ldr	r3, [r5, r3]
+	ldrsh	r3, [r3, r4, lsl #1]
+	vmov	s15, r3	@ int
+	vcvt.f32.s32	s14, s15
+	vldr.32	s13, .L9
+	vdiv.f32	s15, s14, s13
+	vcvt.f64.f32	d7, s15
+	vstr.64	d7, [sp, #8]
+	str	r4, [sp]
+	mov	r3, r1
+	mov	r1, r4
+	ldr	r0, .L9+12
+.LPIC3:
+	add	r0, pc
+	bl	printf(PLT)
+	adds	r0, r4, #1
+	adds	r3, r4, #1
+	ldr	r2, .L9+8
+	ldr	r2, [r5, r2]
+	ldrsh	r3, [r2, r3, lsl #1]
+	mov	r6, r3
+	adds	r3, r4, #1
+	ldr	r2, .L9+8
+	ldr	r2, [r5, r2]
+	ldrsh	r3, [r2, r3, lsl #1]
+	mov	ip, r3
+	adds	r3, r4, #1
+	adds	r2, r4, #1
+	ldr	r1, .L9+8
+	ldr	r1, [r5, r1]
+	ldrsh	r2, [r1, r2, lsl #1]
+	vmov	s15, r2	@ int
+	vcvt.f32.s32	s14, s15
+	vldr.32	s13, .L9
+	vdiv.f32	s15, s14, s13
+	vcvt.f64.f32	d7, s15
+	vstr.64	d7, [sp, #8]
+	str	r3, [sp]
+	mov	r3, ip
+	mov	r2, r6
+	mov	r1, r0
+	ldr	r0, .L9+16
+.LPIC4:
+	add	r0, pc
+	bl	printf(PLT)
+	adds	r0, r4, #2
+	adds	r3, r4, #2
+	ldr	r2, .L9+8
+	ldr	r2, [r5, r2]
+	ldrsh	r3, [r2, r3, lsl #1]
+	mov	r6, r3
+	adds	r3, r4, #2
+	ldr	r2, .L9+8
+	ldr	r2, [r5, r2]
+	ldrsh	r3, [r2, r3, lsl #1]
+	mov	ip, r3
+	adds	r3, r4, #2
+	adds	r2, r4, #2
+	ldr	r1, .L9+8
+	ldr	r1, [r5, r1]
+	ldrsh	r2, [r1, r2, lsl #1]
+	vmov	s15, r2	@ int
+	vcvt.f32.s32	s14, s15
+	vldr.32	s13, .L9
+	vdiv.f32	s15, s14, s13
+	vcvt.f64.f32	d7, s15
+	vstr.64	d7, [sp, #8]
+	str	r3, [sp]
+	mov	r3, ip
+	mov	r2, r6
+	mov	r1, r0
+	ldr	r0, .L9+20
+.LPIC5:
+	add	r0, pc
+	bl	printf(PLT)
+	adds	r0, r4, #3
+	adds	r3, r4, #3
+	ldr	r2, .L9+8
+	ldr	r2, [r5, r2]
+	ldrsh	r3, [r2, r3, lsl #1]
+	mov	r6, r3
+	adds	r3, r4, #3
+	ldr	r2, .L9+8
+	ldr	r2, [r5, r2]
+	ldrsh	r3, [r2, r3, lsl #1]
+	mov	ip, r3
+	adds	r3, r4, #3
+	adds	r2, r4, #3
+	ldr	r1, .L9+8
+	ldr	r1, [r5, r1]
+	ldrsh	r2, [r1, r2, lsl #1]
+	vmov	s15, r2	@ int
+	vcvt.f32.s32	s14, s15
+	vldr.32	s13, .L9
+	vdiv.f32	s15, s14, s13
+	vcvt.f64.f32	d7, s15
+	vstr.64	d7, [sp, #8]
+	str	r3, [sp]
+	mov	r3, ip
+	mov	r2, r6
+	mov	r1, r0
+	ldr	r0, .L9+24
+.LPIC6:
+	add	r0, pc
+	bl	printf(PLT)
+	adds	r4, r4, #4
+.L5:
+	cmp	r4, #101
+	ble	.L6
+	nop
+	adds	r7, r7, #100
+	mov	sp, r7
+	@ sp needed
+	pop	{r4, r5, r6, r7, pc}
+.L10:
+	.align	2
+.L9:
+	.word	1182793728
 	.word	X(GOT)
 	.word	Y(GOT)
-	.word	.LC0-(.LPIC1+4)
 	.word	.LC2-(.LPIC3+4)
-	.word	.LC1-(.LPIC2+4)
+	.word	.LC2-(.LPIC4+4)
+	.word	.LC2-(.LPIC5+4)
+	.word	.LC2-(.LPIC6+4)
 	.size	main, .-main
-	.comm	Y,256,4
-	.comm	X,256,4
-	.section	.rodata.str1.4,"aMS",%progbits,1
-	.align	2
-.LC0:
-	.ascii	"Y[ 0] = %+6hi = 0x%04hX ....... y[ 0] = %8.5f\012\000"
-	.space	1
-.LC1:
-	.ascii	"Y[ 1] = %+6hi = 0x%04hX ....... y[ 1] = %8.5f\012\000"
-	.space	1
-.LC2:
-	.ascii	"Y[%2d] = %+6hi = 0x%04hX ....... y[%2d] = %8.5f\012"
-	.ascii	"\000"
 	.ident	"GCC: (Debian 8.3.0-6) 8.3.0"
 	.section	.note.GNU-stack,"",%progbits
--- filterV6_LoopUnrollThree/filter.s
+++ filterV6_LoopUnrollThree/filter.s
@@ -6,206 +6,464 @@
 	.eabi_attribute 24, 1
 	.eabi_attribute 25, 1
 	.eabi_attribute 26, 2
-	.eabi_attribute 30, 2
+	.eabi_attribute 30, 6
 	.eabi_attribute 34, 1
 	.eabi_attribute 18, 4
 	.file	"filter.c"
 	.text
-	.section	.text.startup,"ax",%progbits
+	.comm	X,256,4
+	.comm	Y,256,4
 	.align	1
-	.p2align 2,,3
-	.global	main
 	.arch armv7-a
+	.syntax unified
+	.thumb
+	.thumb_func
+	.fpu vfpv3-d16
+	.type	filter_init, %function
+filter_init:
+	@ args = 0, pretend = 0, frame = 8
+	@ frame_needed = 1, uses_anonymous_args = 0
+	@ link register save eliminated.
+	push	{r4, r7}
+	sub	sp, sp, #8
+	add	r7, sp, #0
+	str	r0, [r7, #4]
+	str	r1, [r7]
+	ldr	r3, [r7, #4]
+	movw	r2, #32769
+	strh	r2, [r3]	@ movhi
+	ldr	r3, [r7, #4]
+	adds	r3, r3, #2
+	movw	r2, #32769
+	strh	r2, [r3]	@ movhi
+	movs	r4, #2
+	b	.L2
+.L3:
+	mov	r3, r4
+	lsls	r3, r3, #1
+	ldr	r2, [r7, #4]
+	add	r3, r3, r2
+	movw	r2, #32767
+	strh	r2, [r3]	@ movhi
+	adds	r4, r4, #1
+.L2:
+	cmp	r4, #99
+	ble	.L3
+	ldr	r3, [r7]
+	mov	r2, #49152
+	strh	r2, [r3]	@ movhi
+	ldr	r3, [r7]
+	adds	r3, r3, #2
+	mov	r2, #49152
+	strh	r2, [r3]	@ movhi
+	nop
+	adds	r7, r7, #8
+	mov	sp, r7
+	@ sp needed
+	pop	{r4, r7}
+	bx	lr
+	.size	filter_init, .-filter_init
+	.section	.rodata
+	.align	2
+.LC0:
+	.ascii	"Y[ 0] = %+6hi = 0x%04hX ....... y[ 0] = %8.5f\012\000"
+	.align	2
+.LC1:
+	.ascii	"Y[ 1] = %+6hi = 0x%04hX ....... y[ 1] = %8.5f\012\000"
+	.align	2
+.LC2:
+	.ascii	"Y[%2d] = %+6hi = 0x%04hX ....... y[%2d] = %8.5f\012"
+	.ascii	"\000"
+	.text
+	.align	1
+	.global	main
 	.syntax unified
 	.thumb
 	.thumb_func
 	.fpu vfpv3-d16
 	.type	main, %function
 main:
-	@ args = 0, pretend = 0, frame = 16
-	@ frame_needed = 0, uses_anonymous_args = 0
-	push	{r4, r5, r6, r7, r8, r9, r10, fp, lr}
-	mvn	r1, #-2147450880
-	vpush.64	{d8}
-	movw	r4, #32769
-	ldr	r0, .L8+4
-	movt	r4, 32769
-	ldr	r3, .L8+8
+	@ args = 0, pretend = 0, frame = 72
+	@ frame_needed = 1, uses_anonymous_args = 0
+	push	{r4, r5, r6, r7, lr}
+	sub	sp, sp, #92
+	add	r7, sp, #16
+	ldr	r4, .L7+4
 .LPIC0:
-	add	r0, pc
-	sub	sp, sp, #36
-	ldr	r3, [r0, r3]
-	add	r2, r3, #200
-	mov	fp, r3
-	str	r4, [r3], #4	@ unaligned
-.L2:
-	str	r1, [r3], #4	@ unaligned
-	cmp	r3, r2
-	bne	.L2
-	ldr	r3, .L8+12
-	mov	r2, #49152
-	movt	r2, 65535
-	movs	r6, #0
-	movs	r7, #0
-	mov	r5, #-1073692672
-	ldr	r4, [r0, r3]
-	mov	r1, r2
-	ldr	r0, .L8+16
-	movt	r7, 49136
-	ldr	r3, .L8+20
-	mov	r8, #2
-	str	r5, [r4]	@ unaligned
-.LPIC1:
-	add	r0, pc
-	strd	r6, [sp]
-.LPIC3:
-	add	r3, pc
-	str	r3, [sp, #28]
-	bl	printf(PLT)
-	ldrsh	r2, [r4, #2]
-	mov	r9, r4
-	vldr.32	s17, .L8
-	mov	r4, r8
-	ldr	r0, .L8+24
-	mov	r1, r2
-	vmov	s15, r2	@ int
-.LPIC2:
-	add	r0, pc
-	vmov.f32	s16, s17
-	vcvt.f32.s32	s15, s15
-	vmul.f32	s15, s15, s17
+	add	r4, pc
+	movw	r3, #30384
+	strh	r3, [r7, #70]	@ movhi
+	movw	r3, #30384
+	strh	r3, [r7, #68]	@ movhi
+	movw	r3, #30384
+	strh	r3, [r7, #66]	@ movhi
+	movw	r3, #29863
+	strh	r3, [r7, #64]	@ movhi
+	movw	r3, #38103
+	strh	r3, [r7, #62]	@ movhi
+	ldr	r3, .L7+8
+	ldr	r3, [r4, r3]
+	mov	r1, r3
+	ldr	r3, .L7+12
+	ldr	r3, [r4, r3]
+	mov	r0, r3
+	bl	filter_init(PLT)
+	ldr	r3, .L7+8
+	ldr	r3, [r4, r3]
+	ldrsh	r3, [r3]
+	mov	r1, r3
+	ldr	r3, .L7+8
+	ldr	r3, [r4, r3]
+	ldrsh	r3, [r3]
+	mov	r2, r3
+	ldr	r3, .L7+8
+	ldr	r3, [r4, r3]
+	ldrsh	r3, [r3]
+	vmov	s15, r3	@ int
+	vcvt.f32.s32	s14, s15
+	vldr.32	s13, .L7
+	vdiv.f32	s15, s14, s13
 	vcvt.f64.f32	d7, s15
 	vstr.64	d7, [sp]
+	ldr	r3, .L7+16
+.LPIC1:
+	add	r3, pc
+	mov	r0, r3
 	bl	printf(PLT)
-.L3:
-	ldrh	r2, [fp, #4]
-	movw	r1, #30384
-	ldrh	r8, [fp]
-	mov	r5, #8388608
-	ldrh	r3, [r9]
-	mov	lr, #16384
-	smulbb	r2, r2, r1
-	ldrsh	r7, [r9, #2]
-	smlabb	r8, r8, r1, r5
-	movw	r5, #38103
-	smlabb	r3, r3, r5, lr
-	add	r6, r2, #8388608
-	movw	ip, #29863
-	mov	r10, #8192
-	asrs	r6, r6, #24
-	ldrh	r0, [fp, #2]
-	add	r2, r2, #4194304
-	add	fp, fp, #6
-	add	r9, r9, #6
-	str	r3, [sp, #24]
-	uxth	r3, r6
-	smlabb	r6, r7, ip, r10
-	str	r3, [sp, #16]
-	smulbb	r0, r0, r1
-	ldrh	r3, [fp]
-	smlabb	r7, r7, r5, lr
-	asrs	r2, r2, #23
-	str	r6, [sp, #20]
-	movw	r6, #30384
-	smulbb	r6, r3, r6
-	add	r1, r0, #4194304
-	ldr	r3, [sp, #16]
-	add	r0, r0, #8388608
-	add	r1, r3, r1, asr #23
-	ldr	r3, [sp, #20]
-	add	r1, r1, r8, asr #24
-	add	r8, r6, #8388608
-	add	r6, r6, #4194304
-	add	r1, r1, r3, asr #14
-	ldr	r3, [sp, #24]
-	add	r2, r2, r8, asr #24
-	ldrh	r8, [fp, #2]
-	add	r2, r2, r0, asr #24
-	add	r3, r1, r3, asr #15
-	add	r7, r2, r7, asr #15
-	movw	r1, #30384
-	sxth	r3, r3
-	mov	r0, r1	@ movhi
-	mov	r1, #8388608
-	strh	r3, [r9, #-2]	@ movhi
-	vmov	s15, r3	@ int
-	smlabb	r2, r3, ip, r10
-	vcvt.f32.s32	s14, s15
-	smlabb	r8, r8, r0, r1
-	ldr	r0, [sp, #16]
-	smlabb	r5, r3, r5, lr
-	mov	r1, r4
-	add	r7, r7, r2, asr #14
+	ldr	r3, .L7+8
+	ldr	r3, [r4, r3]
+	ldrsh	r3, [r3, #2]
+	mov	r1, r3
+	ldr	r3, .L7+8
+	ldr	r3, [r4, r3]
+	ldrsh	r3, [r3, #2]
 	mov	r2, r3
-	vmul.f32	s14, s14, s16
-	sxth	r7, r7
-	add	r8, r0, r8, asr #24
-	strh	r7, [r9]	@ movhi
-	smlabb	ip, r7, ip, r10
-	add	r6, r8, r6, asr #23
-	ldr	r8, [sp, #28]
-	add	r5, r6, r5, asr #15
-	adds	r7, r4, #1
-	adds	r6, r4, #2
-	mov	r0, r8
-	add	r5, r5, ip, asr #14
-	strh	r5, [r9, #2]	@ movhi
-	str	r4, [sp]
-	adds	r4, r4, #3
-	vcvt.f64.f32	d7, s14
-	vstr.64	d7, [sp, #8]
-	bl	printf(PLT)
-	ldrsh	r3, [r9]
-	mov	r1, r7
-	str	r7, [sp]
-	mov	r0, r8
+	ldr	r3, .L7+8
+	ldr	r3, [r4, r3]
+	ldrsh	r3, [r3, #2]
 	vmov	s15, r3	@ int
-	mov	r2, r3
 	vcvt.f32.s32	s14, s15
-	vmul.f32	s14, s14, s16
-	vcvt.f64.f32	d7, s14
-	vstr.64	d7, [sp, #8]
+	vldr.32	s13, .L7
+	vdiv.f32	s15, s14, s13
+	vcvt.f64.f32	d7, s15
+	vstr.64	d7, [sp]
+	ldr	r3, .L7+20
+.LPIC2:
+	add	r3, pc
+	mov	r0, r3
 	bl	printf(PLT)
-	ldrsh	r3, [r9, #2]
-	mov	r1, r6
-	str	r6, [sp]
-	mov	r0, r8
-	vmov	s15, r3	@ int
-	mov	r2, r3
-	vcvt.f32.s32	s14, s15
-	vmul.f32	s14, s14, s16
-	vcvt.f64.f32	d7, s14
-	vstr.64	d7, [sp, #8]
-	bl	printf(PLT)
-	cmp	r4, #101
-	bne	.L3
-	add	sp, sp, #36
-	@ sp needed
-	vldm	sp!, {d8}
-	pop	{r4, r5, r6, r7, r8, r9, r10, fp, pc}
-.L9:
-	.align	2
+	movs	r5, #2
+	b	.L5
 .L8:
-	.word	947912704
+	.align	2
+.L7:
+	.word	1182793728
 	.word	_GLOBAL_OFFSET_TABLE_-(.LPIC0+4)
+	.word	Y(GOT)
+	.word	X(GOT)
+	.word	.LC0-(.LPIC1+4)
+	.word	.LC1-(.LPIC2+4)
+.L6:
+	ldrsh	r3, [r7, #70]
+	ldr	r2, .L9+4
+	ldr	r2, [r4, r2]
+	ldrsh	r2, [r2, r5, lsl #1]
+	mul	r3, r2, r3
+	add	r3, r3, #8388608
+	asrs	r3, r3, #24
+	str	r3, [r7, #56]
+	ldrsh	r3, [r7, #68]
+	subs	r2, r5, #1
+	ldr	r1, .L9+4
+	ldr	r1, [r4, r1]
+	ldrsh	r2, [r1, r2, lsl #1]
+	mul	r3, r2, r3
+	add	r3, r3, #4194304
+	asrs	r3, r3, #23
+	str	r3, [r7, #52]
+	ldrsh	r3, [r7, #66]
+	subs	r2, r5, #2
+	ldr	r1, .L9+4
+	ldr	r1, [r4, r1]
+	ldrsh	r2, [r1, r2, lsl #1]
+	mul	r3, r2, r3
+	add	r3, r3, #8388608
+	asrs	r3, r3, #24
+	str	r3, [r7, #48]
+	ldrsh	r3, [r7, #62]
+	subs	r2, r5, #2
+	ldr	r1, .L9+8
+	ldr	r1, [r4, r1]
+	ldrsh	r2, [r1, r2, lsl #1]
+	mul	r3, r2, r3
+	add	r3, r3, #16384
+	asrs	r3, r3, #15
+	str	r3, [r7, #44]
+	ldrsh	r3, [r7, #64]
+	subs	r2, r5, #1
+	ldr	r1, .L9+8
+	ldr	r1, [r4, r1]
+	ldrsh	r2, [r1, r2, lsl #1]
+	mul	r3, r2, r3
+	add	r3, r3, #8192
+	asrs	r3, r3, #14
+	str	r3, [r7, #40]
+	ldr	r3, [r7, #56]
+	uxth	r2, r3
+	ldr	r3, [r7, #52]
+	uxth	r3, r3
+	add	r3, r3, r2
+	uxth	r2, r3
+	ldr	r3, [r7, #48]
+	uxth	r3, r3
+	add	r3, r3, r2
+	uxth	r2, r3
+	ldr	r3, [r7, #40]
+	uxth	r3, r3
+	add	r3, r3, r2
+	uxth	r2, r3
+	ldr	r3, [r7, #44]
+	uxth	r3, r3
+	add	r3, r3, r2
+	uxth	r3, r3
+	sxth	r2, r3
+	ldr	r3, .L9+8
+	ldr	r3, [r4, r3]
+	strh	r2, [r3, r5, lsl #1]	@ movhi
+	ldrsh	r3, [r7, #70]
+	adds	r2, r5, #1
+	ldr	r1, .L9+4
+	ldr	r1, [r4, r1]
+	ldrsh	r2, [r1, r2, lsl #1]
+	mul	r3, r2, r3
+	add	r3, r3, #8388608
+	asrs	r3, r3, #24
+	str	r3, [r7, #36]
+	ldrsh	r3, [r7, #68]
+	ldr	r2, .L9+4
+	ldr	r2, [r4, r2]
+	ldrsh	r2, [r2, r5, lsl #1]
+	mul	r3, r2, r3
+	add	r3, r3, #4194304
+	asrs	r3, r3, #23
+	str	r3, [r7, #32]
+	ldrsh	r3, [r7, #66]
+	subs	r2, r5, #1
+	ldr	r1, .L9+4
+	ldr	r1, [r4, r1]
+	ldrsh	r2, [r1, r2, lsl #1]
+	mul	r3, r2, r3
+	add	r3, r3, #8388608
+	asrs	r3, r3, #24
+	str	r3, [r7, #28]
+	ldrsh	r3, [r7, #62]
+	subs	r2, r5, #1
+	ldr	r1, .L9+8
+	ldr	r1, [r4, r1]
+	ldrsh	r2, [r1, r2, lsl #1]
+	mul	r3, r2, r3
+	add	r3, r3, #16384
+	asrs	r3, r3, #15
+	str	r3, [r7, #24]
+	ldrsh	r3, [r7, #64]
+	ldr	r2, .L9+8
+	ldr	r2, [r4, r2]
+	ldrsh	r2, [r2, r5, lsl #1]
+	mul	r3, r2, r3
+	add	r3, r3, #8192
+	asrs	r3, r3, #14
+	str	r3, [r7, #20]
+	ldr	r3, [r7, #36]
+	uxth	r2, r3
+	ldr	r3, [r7, #32]
+	uxth	r3, r3
+	add	r3, r3, r2
+	uxth	r2, r3
+	ldr	r3, [r7, #28]
+	uxth	r3, r3
+	add	r3, r3, r2
+	uxth	r2, r3
+	ldr	r3, [r7, #20]
+	uxth	r3, r3
+	add	r3, r3, r2
+	uxth	r2, r3
+	ldr	r3, [r7, #24]
+	uxth	r3, r3
+	add	r3, r3, r2
+	uxth	r2, r3
+	adds	r3, r5, #1
+	sxth	r1, r2
+	ldr	r2, .L9+8
+	ldr	r2, [r4, r2]
+	strh	r1, [r2, r3, lsl #1]	@ movhi
+	ldrsh	r3, [r7, #70]
+	adds	r2, r5, #2
+	ldr	r1, .L9+4
+	ldr	r1, [r4, r1]
+	ldrsh	r2, [r1, r2, lsl #1]
+	mul	r3, r2, r3
+	add	r3, r3, #8388608
+	asrs	r3, r3, #24
+	str	r3, [r7, #16]
+	ldrsh	r3, [r7, #68]
+	adds	r2, r5, #1
+	ldr	r1, .L9+4
+	ldr	r1, [r4, r1]
+	ldrsh	r2, [r1, r2, lsl #1]
+	mul	r3, r2, r3
+	add	r3, r3, #4194304
+	asrs	r3, r3, #23
+	str	r3, [r7, #12]
+	ldrsh	r3, [r7, #66]
+	ldr	r2, .L9+4
+	ldr	r2, [r4, r2]
+	ldrsh	r2, [r2, r5, lsl #1]
+	mul	r3, r2, r3
+	add	r3, r3, #8388608
+	asrs	r3, r3, #24
+	str	r3, [r7, #8]
+	ldrsh	r3, [r7, #62]
+	ldr	r2, .L9+8
+	ldr	r2, [r4, r2]
+	ldrsh	r2, [r2, r5, lsl #1]
+	mul	r3, r2, r3
+	add	r3, r3, #16384
+	asrs	r3, r3, #15
+	str	r3, [r7, #4]
+	ldrsh	r3, [r7, #64]
+	adds	r2, r5, #1
+	ldr	r1, .L9+8
+	ldr	r1, [r4, r1]
+	ldrsh	r2, [r1, r2, lsl #1]
+	mul	r3, r2, r3
+	add	r3, r3, #8192
+	asrs	r3, r3, #14
+	str	r3, [r7]
+	ldr	r3, [r7, #16]
+	uxth	r2, r3
+	ldr	r3, [r7, #12]
+	uxth	r3, r3
+	add	r3, r3, r2
+	uxth	r2, r3
+	ldr	r3, [r7, #8]
+	uxth	r3, r3
+	add	r3, r3, r2
+	uxth	r2, r3
+	ldr	r3, [r7]
+	uxth	r3, r3
+	add	r3, r3, r2
+	uxth	r2, r3
+	ldr	r3, [r7, #4]
+	uxth	r3, r3
+	add	r3, r3, r2
+	uxth	r2, r3
+	adds	r3, r5, #2
+	sxth	r1, r2
+	ldr	r2, .L9+8
+	ldr	r2, [r4, r2]
+	strh	r1, [r2, r3, lsl #1]	@ movhi
+	ldr	r3, .L9+8
+	ldr	r3, [r4, r3]
+	ldrsh	r3, [r3, r5, lsl #1]
+	mov	r2, r3
+	ldr	r3, .L9+8
+	ldr	r3, [r4, r3]
+	ldrsh	r3, [r3, r5, lsl #1]
+	mov	r1, r3
+	ldr	r3, .L9+8
+	ldr	r3, [r4, r3]
+	ldrsh	r3, [r3, r5, lsl #1]
+	vmov	s15, r3	@ int
+	vcvt.f32.s32	s14, s15
+	vldr.32	s13, .L9
+	vdiv.f32	s15, s14, s13
+	vcvt.f64.f32	d7, s15
+	vstr.64	d7, [sp, #8]
+	str	r5, [sp]
+	mov	r3, r1
+	mov	r1, r5
+	ldr	r0, .L9+12
+.LPIC3:
+	add	r0, pc
+	bl	printf(PLT)
+	adds	r0, r5, #1
+	adds	r3, r5, #1
+	ldr	r2, .L9+8
+	ldr	r2, [r4, r2]
+	ldrsh	r3, [r2, r3, lsl #1]
+	mov	r6, r3
+	adds	r3, r5, #1
+	ldr	r2, .L9+8
+	ldr	r2, [r4, r2]
+	ldrsh	r3, [r2, r3, lsl #1]
+	mov	ip, r3
+	adds	r3, r5, #1
+	adds	r2, r5, #1
+	ldr	r1, .L9+8
+	ldr	r1, [r4, r1]
+	ldrsh	r2, [r1, r2, lsl #1]
+	vmov	s15, r2	@ int
+	vcvt.f32.s32	s14, s15
+	vldr.32	s13, .L9
+	vdiv.f32	s15, s14, s13
+	vcvt.f64.f32	d7, s15
+	vstr.64	d7, [sp, #8]
+	str	r3, [sp]
+	mov	r3, ip
+	mov	r2, r6
+	mov	r1, r0
+	ldr	r0, .L9+16
+.LPIC4:
+	add	r0, pc
+	bl	printf(PLT)
+	adds	r0, r5, #2
+	adds	r3, r5, #2
+	ldr	r2, .L9+8
+	ldr	r2, [r4, r2]
+	ldrsh	r3, [r2, r3, lsl #1]
+	mov	r6, r3
+	adds	r3, r5, #2
+	ldr	r2, .L9+8
+	ldr	r2, [r4, r2]
+	ldrsh	r3, [r2, r3, lsl #1]
+	mov	ip, r3
+	adds	r3, r5, #2
+	adds	r2, r5, #2
+	ldr	r1, .L9+8
+	ldr	r1, [r4, r1]
+	ldrsh	r2, [r1, r2, lsl #1]
+	vmov	s15, r2	@ int
+	vcvt.f32.s32	s14, s15
+	vldr.32	s13, .L9
+	vdiv.f32	s15, s14, s13
+	vcvt.f64.f32	d7, s15
+	vstr.64	d7, [sp, #8]
+	str	r3, [sp]
+	mov	r3, ip
+	mov	r2, r6
+	mov	r1, r0
+	ldr	r0, .L9+20
+.LPIC5:
+	add	r0, pc
+	bl	printf(PLT)
+	adds	r5, r5, #3
+.L5:
+	cmp	r5, #100
+	ble	.L6
+	nop
+	adds	r7, r7, #76
+	mov	sp, r7
+	@ sp needed
+	pop	{r4, r5, r6, r7, pc}
+.L10:
+	.align	2
+.L9:
+	.word	1182793728
 	.word	X(GOT)
 	.word	Y(GOT)
-	.word	.LC0-(.LPIC1+4)
 	.word	.LC2-(.LPIC3+4)
-	.word	.LC1-(.LPIC2+4)
+	.word	.LC2-(.LPIC4+4)
+	.word	.LC2-(.LPIC5+4)
 	.size	main, .-main
-	.comm	Y,256,4
-	.comm	X,256,4
-	.section	.rodata.str1.4,"aMS",%progbits,1
-	.align	2
-.LC0:
-	.ascii	"Y[ 0] = %+6hi = 0x%04hX ....... y[ 0] = %8.5f\012\000"
-	.space	1
-.LC1:
-	.ascii	"Y[ 1] = %+6hi = 0x%04hX ....... y[ 1] = %8.5f\012\000"
-	.space	1
-.LC2:
-	.ascii	"Y[%2d] = %+6hi = 0x%04hX ....... y[%2d] = %8.5f\012"
-	.ascii	"\000"
 	.ident	"GCC: (Debian 8.3.0-6) 8.3.0"
 	.section	.note.GNU-stack,"",%progbits
