a6f5ecb2ba457839e36c2635666474f8607b1954
Brett Dionello <Brettdion@live.ca>
2024-08-02 19:56:01 -0700

    Removed excess line

--- filterV3_ReorderofInstructions/FilterTimed.sh
+++ filterV3_ReorderofInstructions/FilterTimed.sh
@@ -4,5 +4,4 @@ start_time=$(date +%s%N)
 end_time=$(date +%s%N)
 elapsed=$((end_time - start_time))
 echo "Elapsed time: $((elapsed / 1000000)) milliseconds"
-
 # Run this in terminal with: bash FilterTimed.sh
